60f75e334c28093e62816fe8a507b405991d948d
luo <sam9310505@gmail.com>
2024-03-26 15:24:24 +0000

    feat: implement reconnect method

--- index.js
+++ index.js
@@ -6,93 +6,102 @@ const _ = require('lodash');
 const isContainFace = require('./lib/face-detection');
 const SaveGoogleDrive = require('./lib/google-drive');
 
-const ws = new Websocket('wss://gateway.discord.gg/');
-
 const token = process.env.USER_TOKEN;
 const userId = process.env.USER_ID;
 const channelId = process.env.CHANNEL_ID;
 const nickName = process.env.NICKNAME;
 const serverId = process.env.GUILD_ID;
 
-// eslint-disable-next-line no-unused-vars
-let interval = 0;
-const payload = {
-  op: 2,
-  d: {
-    token: token,
-    intents: 513,
-    properties: {
-      $os: 'linux',
-      $browser: 'chrome',
-      $device: 'chrome',
+async function init() {
+  const ws = new Websocket('wss://gateway.discord.gg/');
+  // eslint-disable-next-line no-unused-vars
+  let interval = 0;
+  const payload = {
+    op: 2,
+    d: {
+      token: token,
+      intents: 513,
+      properties: {
+        $os: 'linux',
+        $browser: 'chrome',
+        $device: 'chrome',
+      }
     }
-  }
-};
-
-ws.on('open', async function open() {
-  ws.send(JSON.stringify(payload));
-});
-
-ws.on('message', async function incoming(data) {
-  let payload = JSON.parse(data);
-
-  // https://github.com/meew0/discord-api-docs-1/blob/master/docs/topics/GATEWAY.md
-  const { op, d, t } = payload;
-
-  switch (op) {
-    case 10: {
-      const { heartbeat_interval } = d;
-      interval = heartBeat(heartbeat_interval);
-      break;
-    }
-  }
-
-  // only effect at specify server
-  if (_.get(d, 'guild_id') !== serverId) {
-    return;
-  }
-
-  // image test
-  const attachments = _.map(_.get(d, 'attachments'), 'url');
-  for (const attachment of attachments) {
-    if (!await isContainFace(attachment)) {
-      console.log('[Face Detection]: False');
-      continue;
-    }
-    // Todo: connect with google drive
-    console.log('[Face Detection]: True');
-    const authorId = _.get(d, 'author.id');
-    const authorName = _.get(d, 'author.username');
-    SaveGoogleDrive(attachment, authorId, authorName);
-  }
-  // fix name if changed by others
-  if (t === 'GUILD_MEMBER_UPDATE' && d.user.id === userId) {
-    // Nick name not changed
-    if (d.nick === nickName) {
-      return;
-    }
-    // Reset nick name
-    changeNickName(channelId, nickName);
-  }
-});
-
-// Continue connect with socket
-const heartBeat = (ms) => {
-  return setInterval(() => {
-    ws.send(JSON.stringify({ op: 1, d: null }));
-  }, ms);
-};
-
-// Set Nick Name
-async function changeNickName(channelId, nickName) {
-  const data = JSON.stringify({ 'nick': nickName });
-  const config = {
-    headers: {
-      'Content-Type': 'application/json ',
-      'Authorization': token,
-    },
   };
 
-  await axios.patch(`https://discord.com/api/v9/guilds/${channelId}/members/@me`, data, config);
-  console.log('change nickname');
+  ws.on('open', async function open() {
+    console.log('[Websocket]: Connecting!');
+    ws.send(JSON.stringify(payload));
+  });
+
+  ws.on('message', async function incoming(data) {
+    let payload = JSON.parse(data);
+
+    // https://github.com/meew0/discord-api-docs-1/blob/master/docs/topics/GATEWAY.md
+    const { op, d, t } = payload;
+
+    switch (op) {
+      case 10: {
+        const { heartbeat_interval } = d;
+        interval = heartBeat(heartbeat_interval);
+        break;
+      }
+    }
+
+    // only effect at specify server
+    if (_.get(d, 'guild_id') !== serverId) {
+      return;
+    }
+
+    // image test
+    const attachments = _.map(_.get(d, 'attachments'), 'url');
+    for (const attachment of attachments) {
+      if (!await isContainFace(attachment)) {
+        console.log('[Face Detection]: False');
+        continue;
+      }
+      // Todo: connect with google drive
+      console.log('[Face Detection]: True');
+      const authorId = _.get(d, 'author.id');
+      const authorName = _.get(d, 'author.username');
+      SaveGoogleDrive(attachment, authorId, authorName);
+    }
+    // fix name if changed by others
+    if (t === 'GUILD_MEMBER_UPDATE' && d.user.id === userId) {
+      // Nick name not changed
+      if (d.nick === nickName) {
+        return;
+      }
+      // Reset nick name
+      changeNickName(channelId, nickName);
+    }
+  });
+
+  ws.on('close', async function close() {
+    console.log('[Websocket]: Disconnected!');
+    init();
+  });
+
+  // Continue connect with socket
+  const heartBeat = (ms) => {
+    return setInterval(() => {
+      ws.send(JSON.stringify({ op: 1, d: null }));
+    }, ms);
+  };
+
+  // Set Nick Name
+  async function changeNickName(channelId, nickName) {
+    const data = JSON.stringify({ 'nick': nickName });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json ',
+        'Authorization': token,
+      },
+    };
+
+    await axios.patch(`https://discord.com/api/v9/guilds/${channelId}/members/@me`, data, config);
+    console.log('change nickname');
+  }
 }
+
+init();
